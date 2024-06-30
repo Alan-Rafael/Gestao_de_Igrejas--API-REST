c48d3782fea61ff4c00dba3617742d7b92e6cc16
Alan Bessa <alanrafael20033@gmail.com>
2024-06-30 14:43:59 -0300

    commit to add test-Ministries

--- src/test/java/com/agenda/agendaLagoinha/minitries/MinistryServiceTest.java
+++ src/test/java/com/agenda/agendaLagoinha/minitries/MinistryServiceTest.java
@@ -4,11 +4,13 @@ package com.agenda.agendaLagoinha.minitries;
 import com.agenda.agendaLagoinha.event.Event;
 import com.agenda.agendaLagoinha.member.Member;
 import com.agenda.agendaLagoinha.member.MemberRepository;
+import com.agenda.agendaLagoinha.member.exception.MemberNotFoundException;
 import com.agenda.agendaLagoinha.ministerios.Ministry;
 import com.agenda.agendaLagoinha.ministerios.MinistryRepository;
 import com.agenda.agendaLagoinha.ministerios.MinistryService;
 import com.agenda.agendaLagoinha.requests.CreateMinistryRequest;
 import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,18 +19,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MinistryServiceTest {
 
     @Mock
     private MinistryRepository ministryRepository;
-
     @Mock
     private MemberRepository memberRepository;
     @Mock
@@ -36,47 +35,73 @@ public class MinistryServiceTest {
     @InjectMocks
     private MinistryService ministryService;
 
+    @Nested
+    public  class testes_para_criar_ministerios{
+        @Test
+        void createMinistry(){
+            var adminId = UUID.randomUUID();
 
-    @Test
-    void createMinistry(){
-        var adminId = UUID.randomUUID();
+            Member member1 = new Member();
+            member1.setCpf("09876543211");
+            Member lider = new Member();
+            lider.setCpf("12669321437");
+            Set<Member> members = new HashSet<>();
+            members.add(member1);
 
-        Member member1 = new Member();
-        member1.setCpf("09876543211");
-        Member lider = new Member();
-        lider.setCpf("12669321437");
-        Set<Member> members = new HashSet<>();
-        members.add(member1);
+            var ministryRequest = new CreateMinistryRequest(
+                    "minister test", "12669321437",
+                    List.of("09876543211", "11223344556")
+            );
 
-        var ministryRequest = new CreateMinistryRequest(
-                "minister test",
-                "12669321437",
-                List.of("09876543211", "11223344556")
-        );
+            var minister = Ministry.builder()
+                    .name("minister test").leader(lider)
+                    .adminId(adminId).ministryMembers(null).build();
 
-        var minister = Ministry.builder()
-                .name("minister test")
-                .leader(lider)
-                .adminId(adminId)
-                .ministryMembers(null)
-                .build();
-        lider.addMinisterioQueSouLider(minister);
+            lider.addMinisterioQueSouLider(minister);
 
-        when(memberRepository.findByCpf("12669321437")).thenReturn(lider);
-        when(memberRepository.findByCpfIn(List.of("09876543211", "11223344556"))).thenReturn(members);
-        when(request.getAttribute("admin_id")).thenReturn(adminId.toString());
-        when(ministryRepository.save(any(Ministry.class))).thenAnswer(i -> i.getArguments()[0]);
+            when(memberRepository.findByCpf("12669321437")).thenReturn(lider);
+            when(memberRepository.findByCpfIn(List.of("09876543211", "11223344556"))).thenReturn(members);
+            when(request.getAttribute("admin_id")).thenReturn(adminId.toString());
+            when(ministryRepository.save(any(Ministry.class))).thenAnswer(i -> i.getArguments()[0]);
 
-        minister = ministryService.insert(request, ministryRequest);
+            minister = ministryService.insert(request, ministryRequest);
 
-        assertNotNull(minister);
-        assertEquals("minister test", minister.getName());
-        assertEquals(members, minister.getMinistryMembers());
-        assertEquals(adminId, minister.getAdminId());
+            assertNotNull(minister);
+            assertEquals("minister test", minister.getName());
+            assertEquals(members, minister.getMinistryMembers());
+            assertEquals(adminId, minister.getAdminId());
 
+        }
+
+        @Test
+        public  void criarMinisterioCaseDeErro(){
+
+            Member lider = new Member();
+            lider.setCpf("12669321437");
+
+            var adminId = UUID.randomUUID();
+
+            var ministryRequest = new CreateMinistryRequest(
+                    "minister test",
+                    "12669321437",
+                    List.of("09876543211", "11223344556")
+            );
+
+            var minister = Ministry.builder()
+                    .name("minister test")
+                    .leader(lider)
+                    .adminId(adminId)
+                    .ministryMembers(null).build();
+            lider.addMinisterioQueSouLider(minister);
+
+            when(memberRepository.findByCpf("12669321437")).thenReturn(null);
+
+            assertThrows(MemberNotFoundException.class, () -> {
+                ministryService.insert(request, ministryRequest);
+            });
+
+            verify(memberRepository).findByCpf("12669321437");
+        }
     }
-
-
-
 
 }
