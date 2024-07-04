2e86ea3c5560a55f9ece4dfc86db0e60e6559a0e
Alan Bessa <alanrafael20033@gmail.com>
2024-07-04 18:07:46 -0300

    commit to add Logins o members

--- src/main/java/com/agenda/agendaLagoinha/member/LoginController.java
+++ src/main/java/com/agenda/agendaLagoinha/member/LoginController.java
@@ -3,11 +3,13 @@ package com.agenda.agendaLagoinha.member;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
 
-@RestController
+@RestController()
+@RequestMapping("loginMembers")
 public class LoginController {
 
     final MemberLoginService service;
@@ -15,7 +17,8 @@ public class LoginController {
     public LoginController(MemberLoginService service) {
         this.service = service;
     }
-    @PostMapping
+
+    @PostMapping("entrar")
     public String login(@RequestBody  MemberAuthDto memberAuthDto) throws AuthenticationException {
         return this.service.realizarLogin(memberAuthDto);
     }
